dbc931dcd8411d0ef3462f10772dd09788b446cf
4seokhyeon <tktjrgus@naver.com>
2023-08-16 15:52:23 +0900

    메인 페이지 수정(리스트뷰,item)

--- snsproject/app/src/main/java/com/example/snsproject/ui/activity/MainPageActivity.kt
+++ snsproject/app/src/main/java/com/example/snsproject/ui/activity/MainPageActivity.kt
@@ -3,10 +3,14 @@ package com.example.snsproject.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.snsproject.R
+import com.example.snsproject.manager.MemberManager
+import com.example.snsproject.manager.MemberManagerImpl
 
 class MainPageActivity : AppCompatActivity() {
+    lateinit var memberManager: MemberManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+        memberManager = MemberManagerImpl.getInstance()
     }
 }
