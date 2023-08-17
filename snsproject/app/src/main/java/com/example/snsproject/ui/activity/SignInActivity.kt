455239b5ce97faa3ffd0e34e4b7d6f0a5907bd3f
bkt2790 <qwert9788@hanyang.ac.kr>
2023-08-17 14:25:12 +0900

    signin

--- snsproject/app/src/main/java/com/example/snsproject/ui/activity/SignInActivity.kt
+++ snsproject/app/src/main/java/com/example/snsproject/ui/activity/SignInActivity.kt
@@ -1,7 +1,7 @@
 package com.example.snsproject.ui.activity
 
-import android.annotation.SuppressLint
-import android.graphics.Color
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -16,10 +16,49 @@ import com.example.snsproject.R
 import com.example.snsproject.adapter.MyPageItemAdapter
 import com.example.snsproject.manager.MemberManager
 import com.example.snsproject.manager.MemberManagerImpl
+import com.example.snsproject.model.Post
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var memberManager: MemberManagerImpl
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        memberManager = MemberManagerImpl.getInstance()
+
+        val loginBtn = findViewById<Button>(R.id.signin_login_btn)
+        val signupBtn = findViewById<Button>(R.id.signin_signup_button)
+
+        loginBtn.setOnClickListener {
+            val id = findViewById<EditText>(R.id.signin_id_text).text.toString()
+            val pw = findViewById<EditText>(R.id.signin_pw_text).text.toString()
+
+            if (id.isEmpty() || pw.isEmpty()) {
+                Toast.makeText(this, "빈칸을 입력 해주세요.", Toast.LENGTH_SHORT).show()
+            } else {
+                val member = memberManager.findMember(id)
+
+                if (member == null) {
+                    Toast.makeText(this, "존재하지 않는 사용자입니다. 올바른 아이디를 입력해주세요.", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    if (member.pw != pw) {
+                        Toast.makeText(this, "올바르지 않은 비밀번호 입니다.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainPageActivity::class.java)
+                        intent.putExtra("userId", id)
+                        startActivity(intent)
+                    }
+                }
+            }
+
+        }
+
+        signupBtn.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
