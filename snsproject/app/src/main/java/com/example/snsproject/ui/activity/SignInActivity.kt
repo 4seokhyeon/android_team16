14bd943ad5a026985a5b5a01cba83f6cd0c13160
dagyeom <consult777@naver.com>
2023-08-17 13:59:31 +0900

    feat_signup_textWatcher/2

--- snsproject/app/src/main/java/com/example/snsproject/ui/activity/SignInActivity.kt
+++ snsproject/app/src/main/java/com/example/snsproject/ui/activity/SignInActivity.kt
@@ -1,11 +1,17 @@
 package com.example.snsproject.ui.activity
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
 import com.example.snsproject.R
 import com.example.snsproject.adapter.MyPageItemAdapter
 import com.example.snsproject.manager.MemberManager
